8548054b9f560c8a614585d28f810a69a51d8576
Emile Wong <emilewong@outlook.com>
2021-06-16 11:55:27 +0800

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Emile Wong on 16/6/2021.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
